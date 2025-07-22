41cef62b7fd799221f8def836a8136198be8afb1
Nicolas Vanderesse <nicolas.vanderesse@u-bordeaux.fr>
2025-07-22 17:33:03 +0200

    Segmentation with point prompts. Not great (to be confirmed)

--- src/utils/dataset/addImageBoxes.py
+++ src/utils/dataset/addImageBoxes.py
@@ -17,11 +17,16 @@ def draw_image_masks(image, bounding_boxes, bgr_color=(255,128,0)):
         cv2.rectangle(image, (a_min, b_min), (a_max, b_max), color=tuple(bgr_color), thickness=2)
     return(image)
 
+# Apply SAM on an image with a /list/ of points
+def apply_sam_model_point_list(image, points, sam_model):
+    image = image.astype(np.float32) / 255.0
+    labels = [1] * len(points) # label 1 means that point prompts are located inside the objects to segment (postivide prompt)
+    results = sam_model.predict(image, stream=False, points=points, labels=labels, imgsz = 1024)
+    return results
+
 # Apply SAM on an image with a /list/ of bounding boxes
 def apply_sam_model_bbox_list(image, bounding_boxes, sam_model):
-    image = image.astype(np.float32) / 255.0
-
-    # Perform segmentation using the SAM model
+    image = image.astype(np.float32) / 255.0 #TOCHECK: Is it necessary?
     results = sam_model.predict(image, stream=False, bboxes=bounding_boxes, imgsz = 1024)
     return results
 
@@ -314,9 +319,18 @@ def save_images_and_annotations(tif_path, csv_path, output_folder, box_width, nu
                     annotations.append(f"0 {x_center} {y_center} {width} {height}")
                     # bounding_boxes_list = bounding_boxes # Pourquoi c'est exécuté à chaque itération ?
             else:
-                results = apply_sam_model_bbox_list(img_array, bounding_boxes, sam_model) # Results est une liste, avec un élément par image (donc 1 element dans notre cas)
-
-                for pred_box in results[0].boxes:
+                #results = apply_sam_model_bbox_list(img_array, bounding_boxes, sam_model) # Results est une liste, avec un élément par image (donc 1 element dans notre cas)
+                # TEMP: get the centers of the boxes and pass them as a prompt (awkward because we already have the point annotations)
+                centers = []
+                for bbox in bounding_boxes:
+                    x_center = (bbox[0] + bbox[2]) // 2
+                    y_center = (bbox[1] + bbox[3]) // 2
+                    centers.append([x_center, y_center])
+                print(idx, " ---------")
+                print(centers[0])
+                results = apply_sam_model_point_list(img_array, centers, sam_model)
+                print(results[0].boxes[0].xywh)
+                for pred_box in results[0].boxes:# Results est une liste, avec un élément par image (donc 1 element dans notre cas)
                     x_center, y_center, width, height = pred_box.xyxyn[0] # YOLO format normalized bounding box
                     annotations.append(f"0 {x_center} {y_center} {width} {height}")
                     bounding_boxes_list.append(pred_box.xyxy[0].int().tolist()) # integer pixel format x1, y1, x2, y2
